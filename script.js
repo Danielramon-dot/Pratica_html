00e91da0a41d40cba730c09f1ed5dfd937064993
Danielramon-dot <danielramonalvesmatias2020@gmail.com>
2026-06-23 09:20:50 -0300

    quase finalizado

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-Console.log("Olá, mundo! Este é o meu projeto.");
+function abrirMenu() {
+  document.getElementById("sidebar").classList.add("active");
+}
+
+function fecharMenu() {
+  document.getElementById("sidebar").classList.remove("active");
+}
+
+function alternarTema() {
+  document.body.classList.toggle("dark-mode");
+}
